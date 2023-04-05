355cb369b1b40ccdb23454de124bdb516c8340cc
Logan Vu <logan.vu@mrjaffesclass.com>
2023-04-05 12:47:26 -0700

    first commit

---
+++ src/main/java/com/mycompany/tictactoe/TicTacToe.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tictactoe;
+
+/**
+ *
+ * @author student
+ */
+public class TicTacToe {
+
+    public static void main(String[] args) {
+       TicTacToe ttt = new TicTacToe();
+    }
+    
+    public TicTacToe() {
+    View view = new View();
+    view.setVisible(true);
+}
+}
